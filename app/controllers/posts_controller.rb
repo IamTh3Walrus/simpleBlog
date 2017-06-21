58a51a170e757e3b23e80ac23502ebcc2dce2895
Stephen Doe <redcard1016@yahoo.com>
2017-06-21 17:26:03 -0400

    Basic struture completed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,50 @@
 class PostsController < ApplicationController
-  def index
+  http_basic_authenticate_with name: "Stephen", password:"4321", except:[:index, :show]
 
+  def index
+    #List the posts on the index page from our post model.
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def new
-
+      @post = Post.new
   end
 
   def create
       #Creates the new post
       @post = Post.new(post_params)
       #Saves the post then redirects it to post.
-      @post.save
-      redirect_to @post
+      if(@post.save)
+        redirect_to @post
+      else
+        render 'new'
+      end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if(@post.update(post_params))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    redirect_to posts_path
+  end
+  #Sets the post method to permit a title and body to be submitted.
   private def post_params
       params.require(:post).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 # Sets the root view for the main page to our post index. Goes to the posts controller and the index.html.erb view.
-root 'posts#index'
+# as: adds the about path to nav link.
+root 'posts#index', as: 'home'
 
-# Sets a regular route. Goes to the pages controller and the about.html.erb view.
-get 'about' => 'pages#about'
+# Sets a regular route. Goes to the pages controller and the about.html.erb view. as: adds the about path to nav link.
+get 'about' => 'pages#about', as: 'about'
 
 # Turns posts into a resourse. Then on command line run rake routes to create additional links.
-resources :posts
+resources :posts do
+  resources :comments
+end
 end
